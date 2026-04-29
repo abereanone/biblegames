4cf4ec66bc07c324d0addbd7985127f840a92db6
Michael A. Coughlin <abereanone@pm.me>
2026-04-29 08:31:29 -0400

    added wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import path from "node:path";
 
 export default defineConfig({
-  site: "https://biblegames.local",
+  site: "https://biblegames.church",
   vite: {
     resolve: {
       alias: {
